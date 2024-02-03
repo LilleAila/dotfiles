c38d02b62ee5a9b6eb41777b9eaa30ddea3b9476
LilleAila <olai.solsvik@gmail.com>
2024-02-03 10:56:48 +0100

    Disabled power button

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -14,6 +14,10 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+	services.logind.extraConfig = /*conf*/ ''
+	HandlePowerKey=ignore
+	'';
+
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
