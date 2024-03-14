5f32aca5abfbe175d17c78d7391cef40f50c00d9
LilleAila <olai.solsvik@gmail.com>
2024-03-14 16:28:59 +0100

    Workspace color-coding for monitors

--- home/programs/wm/ags/windows/bar.ts
+++ home/programs/wm/ags/windows/bar.ts
@@ -45,6 +45,10 @@ const Workspaces = () => Widget.Box({
 			setup: self => self.hook(Hyprland, () => {
 					self.toggleClassName("active", Hyprland.active.workspace.id === id)
 					self.toggleClassName("occupied", (Hyprland.getWorkspace(id)?.windows || 0) > 0)
+					self.toggleClassName("mon0", (Hyprland.getWorkspace(id)?.monitorID || 0) == 0)
+					self.toggleClassName("mon1", (Hyprland.getWorkspace(id)?.monitorID || 0) == 1)
+					self.toggleClassName("mon2", (Hyprland.getWorkspace(id)?.monitorID || 0) == 2)
+					self.toggleClassName("mon3", (Hyprland.getWorkspace(id)?.monitorID || 0) == 3)
 			}),
 		});
 	}),
