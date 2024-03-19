5c83f85765f1b15e18421437d83f6c0b2d56961e
LilleAila <olai.solsvik@gmail.com>
2024-03-19 19:39:50 +0100

    Added volume bar to ags bar

--- home/programs/wm/ags/windows/bar.ts
+++ home/programs/wm/ags/windows/bar.ts
@@ -119,6 +119,39 @@ const SysTray = () => Widget.Box({
 	// .filter(({ id }) => !ignore.value.includes(id))
 	.map(SysTrayItem))
 
+const volumeIcons = {
+	101: "overamplified",
+	67: "high",
+	34: "medium",
+	1: "low",
+	0: "muted",
+};
+const getVolumeIcon = () => {
+	const icon = Audio.speaker.is_muted ? 0 : [101, 67, 34, 1, 0].find(
+		threshold => threshold <= Audio.speaker.volume * 100) || 0;
+	return `audio-volume-${volumeIcons[icon]}-symbolic`
+}
+const VolumeIcon = () => Widget.Icon({
+	class_name: "volume-icon",
+	icon: Utils.watch(getVolumeIcon(), Audio.speaker, getVolumeIcon),
+});
+const VolumeSlider = () => Widget.Slider({
+	class_name: "volume-slider",
+	hexpand: true,
+	draw_value: false,
+	on_change: ({ value }) => Audio.speaker.volume = value,
+	setup: self => self.hook(Audio.speaker, () => {
+		self.value = Audio.speaker.volume || 0
+	}),
+})
+const Volume = () => Widget.Box({
+	class_name: "volume",
+	children: [
+		VolumeIcon(),
+		VolumeSlider(),
+	],
+});
+
 const Start = () => Widget.Box({
     hexpand: true,
     hpack: "start",
@@ -138,6 +171,7 @@ const End = () => Widget.Box({
 		hpack: "end",
 		children: [
 			SysTray(),
+			Volume(),
 			BatteryStatus(),
 		],
 	});
