a02abedb2dc1625df29240dbc6effe5618c9b18c
LilleAila <olai.solsvik@gmail.com>
2024-03-22 18:24:08 +0100

    Added more bookmarks and disabled translation

--- home/programs/browser.nix
+++ home/programs/browser.nix
@@ -78,6 +78,8 @@
 
 			"browser.tabs.firefox-view" = false;
 			"browser.tabs.tabmanager.enabled" = false;
+
+			"browser.translations.neverTranslateLanguages" = "nb,nn,fr,en";
 		};
 
 		# All available extensions:
@@ -149,6 +151,23 @@
 						{ name = "Docs"; url = "https://docs.google.com"; }
 						{ name = "Slides"; url = "https://slides.google.com"; }
 						{ name = "Sheets"; url = "https://sheets.google.com"; }
+						{ name = "Drive"; url = "https://drive.google.com"; }
+						{
+							name = "Documents";
+							bookmarks = [
+								{ name = "English"; url = "https://docs.google.com/document/d/1xj9lDLwrGXKkkqr5mEVQLZdQxfKjBSPiBl9a9aHrhNQ/edit"; }
+								{ name = "Nynorsk"; url = "https://docs.google.com/document/d/1hF0peeWeIx4bNYaykgMdfm3YrZyJsipjhb_6KqQueeg/edit"; }
+								{ name = "Norsk"; url = "https://docs.google.com/document/d/1bKgBIAGnONUg9TsEbsb8hGr2u6LgrHCrLBAUk1Ivv-s/edit"; }
+								{ name = "KRLE"; url = "https://docs.google.com/document/d/1wWP4jjaiFDXDRO1LBMBmbOSPSd0ZXMbhcNb9vgMWK44/edit"; }
+								{ name = "Samfunnsfag"; url = "https://docs.google.com/document/d/1AMq0_-sWujxhk5OspIRB4MrFLWIe2Izvn1I_e1T9rhg/edit"; }
+								{ name = "Fransk"; url = "https://docs.google.com/document/d/1ttxzRWYRL_4W4kixq78qgt09NqbmVisZcHJ6_FCb99Q/edit"; }
+								{ name = "Musikk"; url = "https://docs.google.com/document/d/10DauaVT-wLxNg3A8VgM5a4zAvoLF7vCqhhVLZeEIEe8/edit"; }
+								{ name = "Matte"; url = "https://docs.google.com/document/d/1jsF9bZ8rUd8sUd75SlvpIqlNVzKF6kBdeogEvenct0U/edit"; }
+								{ name = "Matte regneark"; url = "https://docs.google.com/spreadsheets/d/19JvJXjdEXP_HnbMi89cvK6Kb8yT-gQsTk6U_P4c7k6Q/edit"; }
+								{ name = "Naturfag"; url = "https://docs.google.com/document/d/1MkoYDXgKMk9SbBmSpr2izJ9iBOjOAlryh-UeayjFj0M/edit"; }
+								{ name = "K&H"; url = "https://docs.google.com/document/d/1ISrUL7hzWwBrSdELAFkYC2CBjyvx_rWS9s5M53TG9AM/edit"; }
+							];
+						}
 					];
 				}];
 				userChrome = with config.colorScheme.palette; colorTab base0B base00;
