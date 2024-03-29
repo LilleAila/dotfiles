2f069ca945ba6fe8bb7a94e8f6a5cfde51c183dc
LilleAila <olai.solsvik@gmail.com>
2024-03-29 17:31:59 +0100

    Made active workspce wider

--- home/programs/shell/terminal.nix
+++ home/programs/shell/terminal.nix
@@ -42,7 +42,7 @@
           allow_remote_control = "yes";
           enable_audio_bell = "no";
           copy_on_select = "clipboard";
-          background_opacity = "0.7";
+          background_opacity = "0.8";
 
           foreground = "#${base06}";
           background = "#${base00}";
