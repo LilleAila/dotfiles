a3e2dd9a9e21ae08a6c25023885267fb579b75a6
LilleAila <olai.solsvik@gmail.com>
2024-04-05 17:30:53 +0200

    Upgraded system, close #6

--- flake.nix
+++ flake.nix
@@ -4,8 +4,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    nixpkgs-staging.url = "github:nixos/nixpkgs/staging-next";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
