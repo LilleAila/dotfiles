517ba2221789b78a7579cb24ad208e965d2f6459
LilleAila <olai.solsvik@gmail.com>
2024-04-16 15:25:14 +0200

    Zellij colors

--- home/programs/shell/zellij.nix
+++ home/programs/shell/zellij.nix
@@ -9,16 +9,33 @@
     {
       programs.zellij = {
         enable = true;
-        settings = {};
+        settings = {
+          themes.nix-colors = let
+            c = config.colorScheme.colors;
+          in {
+            fg = "#${c.base06}";
+            bg = "#${c.base00}";
+            red = "#${c.base08}";
+            green = "#${c.base0B}";
+            yellow = "#${c.base0A}";
+            blue = "#${c.base0D}";
+            magenta = "#${c.base0E}";
+            orange = "#${c.base09}";
+            cyan = "#${c.base0C}";
+            black = "#${c.base00}";
+            white = "#${c.base05}";
+          };
+          theme = "nix-colors";
+        };
+      };
+      home.shellAliases = {
+        zj = "zellij";
       };
     }
-    (lib.mkIf (config.settings.terminal.emulator.name == "kitty") {
-      # programs.zsh.initExtra = ''
-      #   ${lib.getExe config.programs.zellij.package}
-      # '';
-      programs.kitty.settings.shell = ''
-        ${lib.getExe config.programs.zellij.package}
-      '';
-    })
+    # (lib.mkIf (config.settings.terminal.emulator.name == "kitty") {
+    #   programs.kitty.settings.shell = ''
+    #     ${lib.getExe config.programs.zellij.package}
+    #   '';
+    # })
   ]);
 }
