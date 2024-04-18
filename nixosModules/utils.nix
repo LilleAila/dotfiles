684291623626868faaf55af623de63b8d4deffad
LilleAila <olai.solsvik@gmail.com>
2024-04-18 16:38:36 +0200

    Added polkit-gnome systemd service

--- nixosModules/utils.nix
+++ nixosModules/utils.nix
@@ -31,6 +31,20 @@
 
     security.polkit.enable = true;
 
+    systemd.user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = ["graphical-session.target"];
+      wants = ["graphical-session.target"];
+      after = ["graphical-session.target"];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+
     # system.nixos.codeName = "hmm";
 
     # Enable some packages and random stuff
