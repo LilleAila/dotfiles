1d7d8bc99180124d29ca60ca48d9faf21d17234b
LilleAila <olai.solsvik@gmail.com>
2024-04-23 17:39:22 +0200

    Cleaned up pkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,11 +1,7 @@
 {pkgs ? import <nixpkgs> {}}: {
-  imports = [
-    ./cursors
-  ];
-
   box64 = pkgs.callPackage ./box64.nix {};
   fhsenv = pkgs.callPackage ./fhsenv.nix {};
 
   # Doesn't work :(
-  nerdfont_patcher = pkgs.callPackage ./nerdfonts.nix {};
+  # nerdfont_patcher = pkgs.callPackage ./nerdfonts.nix {};
 }
