777c50ba7d269b9cd25838042eebd88a48d7bbb5
LilleAila <olai.solsvik@gmail.com>
2024-04-28 17:09:17 +0200

    T420: refactored home config

--- home/t420.nix
+++ home/t420.nix
@@ -7,8 +7,6 @@
 }: {
   imports = [./.];
 
-  # https://github.com/tinted-theming/base16-schemes/
-  colorScheme = inputs.nix-colors.colorSchemes.gruvbox-dark-medium;
   settings = {
     monitors = [
       {
@@ -16,45 +14,7 @@
         wallpaper = ./wallpapers/wall13.jpg;
       }
     ];
-    qt.enable = true;
-    gtk.enable = true;
-    wm = {
-      ags.enable = true;
-      hyprland = {
-        enable = true;
-        screenshots.enable = true;
-      };
-      sway = {
-        enable = true;
-      };
-      avizo.enable = true;
-      hypridle.enable = true;
-      swaylock.enable = true;
-      hyprpaper.enable = true;
-      mako.enable = true;
-    };
-    files.nemo.enable = true;
-    zathura.enable = true;
-    browser = {
-      firefox.enable = true;
-      firefox.newtab_image = ./wallpapers/wall10.jpg;
-      qutebrowser.enable = true;
-    };
-    discord.vesktop.enable = true;
-    terminal = {
-      zsh = {
-        enable = true;
-        theme = "nanotech";
-      };
-      utils.enable = true;
-      emulator = {
-        enable = true;
-        name = "kitty";
-      };
-      neovim.enable = true;
-    };
-    imageviewer.enable = true;
-    other.enable = true;
+    desktop.enable = true;
   };
 
   wayland.windowManager.hyprland.settings.input.kb_options = lib.mkForce "ctrl:nocaps,altwin:swap_lalt_lwin";
