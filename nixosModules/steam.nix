df5fe740559738288829fa7693d31138709541b3
LilleAila <olai.solsvik@gmail.com>
2024-05-02 18:58:52 +0200

    System: added gaming enable option

--- nixosModules/steam.nix
+++ nixosModules/steam.nix
@@ -6,19 +6,28 @@
   ...
 }: {
   options.settings.steam.enable = lib.mkEnableOption "steam";
+  options.settings.gaming.enable = lib.mkEnableOption "gaming";
 
-  config = lib.mkIf config.settings.steam.enable {
-    programs.steam = {
-      enable = true;
-      package = pkgs.steam;
-      remotePlay.openFirewall = true;
-      dedicatedServer.openFirewall = true;
-    };
+  config = lib.mkMerge [
+    (lib.mkIf config.settings.steam.enable {
+      programs.steam = {
+        enable = true;
+        package = pkgs.steam;
+        remotePlay.openFirewall = true;
+        dedicatedServer.openFirewall = true;
+      };
 
-    settings.nix.unfree = [
-      "steam"
-      "steam-original"
-      "steam-run"
-    ];
-  };
+      settings.nix.unfree = [
+        "steam"
+        "steam-original"
+        "steam-run"
+      ];
+
+      settings.gaming.enable = true;
+    })
+    (lib.mkIf config.settings.gaming.enable {
+      programs.gamemode.enable = true;
+      programs.gamescope.enable = true;
+    })
+  ];
 }
