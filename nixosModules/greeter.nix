be6d1c57023c0870e648b891f4ca68d0554266a1
LilleAila <olai.solsvik@gmail.com>
2024-05-08 19:33:44 +0200

    TTY: Set ansi colors with nix-colors, Tuigreet: configure some options

--- nixosModules/greeter.nix
+++ nixosModules/greeter.nix
@@ -16,7 +16,32 @@
       enable = true;
       settings = {
         default_session = {
-          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --cmd Hyprland";
+          # command = "${pkgs.greetd.tuigreet}/bin/tuigreet --cmd Hyprland";
+          command =
+            pkgs.writeScript "start_tuigreet"
+            ''
+              ${lib.getExe pkgs.greetd.tuigreet} --cmd Hyprland \
+                --time \
+                --user-menu \
+                --asterisks \
+                --container-padding 2 \
+                --window-padding 2 \
+                --remember \
+                # --theme ${lib.concatStringsSep ";" [
+                # FIXME: Uncomment above line whenever https://github.com/apognu/tuigreet/commit/71cf19e233f990f39b11716a9849c0cf8035b9c3 gets added to nixpkgs
+                # Colors for the TTY are defined in nixosModules/utils.nix
+                "text=white"
+                "time=white"
+                "container=darkgray"
+                "border=green"
+                "title=magenta"
+                "greet=magenta"
+                "prompt=white"
+                "input=gray"
+                "action=gray"
+                "button=cyan"
+              ]}
+            '';
         };
       };
       vt = 2;
