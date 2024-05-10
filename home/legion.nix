faa39cc6d0ddcc334a6f8363f60837b0fd05f632
LilleAila <olai.solsvik@gmail.com>
2024-05-10 17:47:12 +0200

    Legion: changed osbuild alias to build the active specialisation

--- home/legion.nix
+++ home/legion.nix
@@ -33,6 +33,18 @@
     bat-fullcharge = "sudo tlp fullcharge";
     bat-limit = "sudo tlp setcharge 0 1 BAT0";
     bt = "bluetooth";
+    osbuild = let
+      base_cmd = "sudo nixos-rebuild switch --flake ${config.home.homeDirectory}/dotfiles";
+    in
+      (pkgs.writeShellScript "osbuild" ''
+        #!/usr/bin/env bash
+        if [ -z "''${NIXOS_ACTIVE_SPECIALISATION}" ] || [ "''${NIXOS_ACTIVE_SPECIALISATION}" = "default" ]; then
+            ${base_cmd}
+        else
+            ${base_cmd} --specialisation "''${NIXOS_ACTIVE_SPECIALISATION}"
+        fi
+      '')
+      .outPath;
   };
   home.packages = with pkgs; [
     _1password-gui-beta
