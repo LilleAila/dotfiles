85d6aa35fb7a74c80894184f5ecec2a117770262
LilleAila <olai.solsvik@gmail.com>
2024-05-19 15:29:29 +0200

    Flake: tried adding an install script

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# nix-shell -p nixFlakes
-
-# Copy the public key and open firefox to authorize with github
-# nix run nixpkgs#cage -- -s nix run nixpkgs#firefox -- https://github.com/settings/keys & sleep 2; cat ~/.ssh/id_ed25519 | nix shell nixpkgs#wl-clipboard -c wl-copy
-nix-shell -p cage firefox wl-clipboard --run 'cage -s firefox https://github.com/settings/keys & sleep 2; cat ~/.ssh/id_ed25519 | wl-copy'
--- pkgs/install.nix
+++ pkgs/install.nix
@@ -4,4 +4,9 @@
   ...
 }:
 pkgs.writeShellScriptBin "install.sh" ''
+  #!/usr/bin/env bash
+  set -e
+
+  # Authorize the computer with git
+  ${lib.getExe pkgs.cage} -s ${lib.getExe pkgs.firefox} https://github.com/settings/keys & sleep 2; cat ̃~/.ssh/id_ed25519.pub | ${lib.getExe' pkgs.wl-clipboard "wl-copy"}
 ''
