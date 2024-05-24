69a2d3183f1601fabc5b767fc77966bc8a029da7
LilleAila <olai.solsvik@gmail.com>
2024-05-24 18:21:37 +0200

    E14: set up tlp

--- hosts/e14g5/configuration.nix
+++ hosts/e14g5/configuration.nix
@@ -80,4 +80,37 @@
   };
 
   services.fwupd.enable = true;
+
+  services.tlp = {
+    enable = true;
+    settings = {
+      DEVICES_TO_DISABLE_ON_STARTUP = "bluetooth";
+      START_CHARGE_THRESH_BAT0 = 75;
+      STOP_CHARGE_THRESH_BAT0 = 80;
+      RESTORE_THRESHOLDS_ON_BAT = 1;
+
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      PLATFORM_PROFILE_ON_AC = "performance";
+      PLATFORM_PROFILE_ON_BAT = "low-power";
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+      CPU_HWP_DYN_BOOST_ON_AC = 1;
+      CPU_HWP_DYN_BOOST_ON_BAT = 0;
+      RUNTIME_PM_ON_AC = "on";
+      RUNTIME_PM_ON_BAT = "auto";
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 30;
+
+      USB_AUTOSUSPEND = 1;
+      USB_EXCLUDE_AUDIO = 1;
+      WOL_DISABLE = "Y";
+      WIFI_PWR_ON_AC = "off";
+      WIFI_PWR_ON_BAT = "on";
+      DEVICES_TO_DISABLE_ON_LAN_CONNECT = "wifi";
+      DEVICES_TO_ENABLE_ON_LAN_DISCONNECT = "wifi";
+    };
+  };
 }
