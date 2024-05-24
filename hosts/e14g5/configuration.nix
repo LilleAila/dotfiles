e3df1750600397b8760eebbbc6abf916f2af02bc
LilleAila <olai.solsvik@gmail.com>
2024-05-24 17:57:20 +0200

    E14: updated kernel

--- hosts/e14g5/configuration.nix
+++ hosts/e14g5/configuration.nix
@@ -48,6 +48,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.timeout = 2;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   services.logind.extraConfig = ''
     HandleLidSwitch=suspend
