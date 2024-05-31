6660846b863fb54d598cf3e7c6ee7b801bbddbd7
LilleAila <olai.solsvik@gmail.com>
2024-05-31 17:36:19 +0200

    ZSH: updated run command to allow args

--- home/modules/shell/zsh.nix
+++ home/modules/shell/zsh.nix
@@ -27,32 +27,32 @@ in {
         theme = config.settings.terminal.zsh.theme;
       };
       initExtra = ''
-        	ex = () {
-        		if [ -f $1 ] ; then
-        			case $1 in
-        				*.tar.bz2) tar xjf $1 ;;
-        				*.tar.gz)  tar xzf $1 ;;
-        				*.bz2)     bunzip2 $1 ;;
-        				*.rar)     ${pkgs.unrar-wrapper}/bin/unrar x $1 ;;
-        				*.gz)      gunzip $1 ;;
-        				*.tar)     tar xf $1 ;;
-        				*.tbz2)    tar xjf $1 ;;
-        				*.tgz)     tar xzf $1 ;;
-        				*.zip)     ${pkgs.unzip}/bin/unzip $1 ;;
-        				*.Z)       uncompress $1 ;;
-        				*.7z)      ${pkgs.p7zip}/bin/7z x $1 ;;
-        				*.deb)     ar x $1 ;;
-        				*.tar.xz)  tar xf $1 ;;
-        				*.tar.zst) unzstd $1 ;;
-        				*)         echo "Could not extract $1"
-        			esac
-        		else
-        			echo "$1 is not a valid file"
-        		fi
+        ex = () {
+          if [ -f $1 ] ; then
+            case $1 in
+              *.tar.bz2) tar xjf $1 ;;
+              *.tar.gz)  tar xzf $1 ;;
+              *.bz2)     bunzip2 $1 ;;
+              *.rar)     ${pkgs.unrar-wrapper}/bin/unrar x $1 ;;
+              *.gz)      gunzip $1 ;;
+              *.tar)     tar xf $1 ;;
+              *.tbz2)    tar xjf $1 ;;
+              *.tgz)     tar xzf $1 ;;
+              *.zip)     ${pkgs.unzip}/bin/unzip $1 ;;
+              *.Z)       uncompress $1 ;;
+              *.7z)      ${pkgs.p7zip}/bin/7z x $1 ;;
+              *.deb)     ar x $1 ;;
+              *.tar.xz)  tar xf $1 ;;
+              *.tar.zst) unzstd $1 ;;
+              *)         echo "Could not extract $1"
+            esac
+          else
+            echo "$1 is not a valid file"
+          fi
         }
 
         run() {
-          NIXPKGS_ALLOW_UNFREE=1 NIXPKGS_ALLOW_UNSUPPORTED_SYSTEM=1 nix run nixpkgs#$1 --impure
+          NIXPKGS_ALLOW_UNFREE=1 NIXPKGS_ALLOW_UNSUPPORTED_SYSTEM=1 nix run nixpkgs#$1 --impure -- $@
         }
       '';
       /*
