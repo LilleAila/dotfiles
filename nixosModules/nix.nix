80f2f7d75223785ba0757d96582454438817767c
LilleAila <olai.solsvik@gmail.com>
2024-06-04 14:22:18 +0200

    Nix: added more cachix substituters

--- nixosModules/nix.nix
+++ nixosModules/nix.nix
@@ -34,9 +34,17 @@
 
         substituters = [
           "https://hyprland.cachix.org"
+          "https://nix-gaming.cachix.org"
+          "https://anyrun.cachix.org"
+          "https://nix-community.cachix.org"
+          "https://yazi.cachix.org"
         ];
         trusted-public-keys = [
           "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+          "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4"
+          "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
         ];
       };
 
