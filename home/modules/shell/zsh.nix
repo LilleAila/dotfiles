88c691696f4b3fd652cc80e5dd0aba3252a019a0
LilleAila <olai.solsvik@gmail.com>
2024-06-06 18:42:59 +0200

    Zsh: renamed run_ to Run

--- home/modules/shell/zsh.nix
+++ home/modules/shell/zsh.nix
@@ -55,7 +55,7 @@ in {
           NIXPKGS_ALLOW_UNFREE=1 NIXPKGS_ALLOW_UNSUPPORTED_SYSTEM=1 nix run nixpkgs#$1 --impure -- ''${@:2}
         }
 
-        run_() {
+        Run() {
           NIXPKGS_ALLOW_UNFREE=1 NIXPKGS_ALLOW_UNSUPPORTED_SYSTEM=1 nix shell nixpkgs#$1 --impure -c ''${@:2}
         }
       '';
