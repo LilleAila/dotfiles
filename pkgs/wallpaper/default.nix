19bc2588526ed9558c24807316d7edc819f9e85c
LilleAila <olai.solsvik@gmail.com>
2024-06-08 11:32:06 +0200

    Wallpaper: refactored base color and tweaked outer

--- pkgs/wallpaper/default.nix
+++ pkgs/wallpaper/default.nix
@@ -15,15 +15,17 @@ stdenv.mkDerivation {
   buildPhase = let
     c = colorScheme.palette;
     colors = import ../../lib/colors.nix lib;
+    baseColor = c.base0D;
   in ''
     cp $src ./template.svg
-    sed -i "s/#bbbbbb/${colors.format_darken c.base0D 0.9}/g" template.svg
-    sed -i "s/#999999/${colors.format_darken c.base0D 0.7}/g" template.svg
-    sed -i "s/#777777/${colors.format_darken c.base0D 0.5}/g" template.svg
-    sed -i "s/#555555/${colors.format_darken c.base0D 0.4}/g" template.svg
-    sed -i "s/#333333/${colors.format_darken c.base0D 0.3}/g" template.svg
-    sed -i "s/#222222/${colors.format_darken c.base0D 0.2}/g" template.svg
-    # sed -i "s/#222222/#${c.base00}/g" template.svg
+    sed -i "s/#bbbbbb/#${c.base00}/g" template.svg
+    sed -i "s/#999999/#${c.base00}/g" template.svg
+    # sed -i "s/#bbbbbb/${colors.format_darken baseColor 0.9}/g" template.svg
+    # sed -i "s/#999999/${colors.format_darken baseColor 0.7}/g" template.svg
+    sed -i "s/#777777/${colors.format_darken baseColor 0.5}/g" template.svg
+    sed -i "s/#555555/${colors.format_darken baseColor 0.4}/g" template.svg
+    sed -i "s/#333333/${colors.format_darken baseColor 0.3}/g" template.svg
+    sed -i "s/#222222/${colors.format_darken baseColor 0.2}/g" template.svg
 
     sed -i "s/#ffffff/#${c.base00}/g" template.svg
     sed -i "s/#dddddd/#${c.base01}/g" template.svg
