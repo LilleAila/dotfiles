c240031a44db84cbe809e745db06d629ff396255
LilleAila <olai.solsvik@gmail.com>
2024-06-09 15:47:28 +0200

    Webdav: configured https and domain name

--- nixosModules/services/webdav.nix
+++ nixosModules/services/webdav.nix
@@ -13,13 +13,41 @@
       group = "webdav";
     };
 
-    networking.firewall.allowedTCPPorts = [8080];
-    networking.firewall.allowedUDPPorts = [8080];
+    services.nginx = {
+      enable = true;
+      recommendedProxySettings = true;
+      recommendedTlsSettings = true;
+      virtualHosts."webdav.olai.dev" = {
+        enableACME = true;
+        forceSSL = true;
+        locations."/" = {
+          # proxyPass = "http://158.179.205.169:8080";
+          proxyPass = "http://127.0.0.1:8080";
+          # proxyPass = "http://0.0.0.0:8080";
+          # proxyPass = "http://[::1]:8080";
+          extraConfig = ''
+            proxy_ssl_server_name on;
+            proxy_pass_header Authorization;
+          '';
+        };
+      };
+    };
+
+    security.acme = {
+      acceptTerms = true;
+      defaults.email = "olai.solsvik@gmail.com";
+    };
+
+    # idk which ones are *actually* needed
+    networking.firewall.allowedTCPPorts = [8080 443 80];
+    networking.firewall.allowedUDPPorts = [8080 443 80];
 
     services.webdav-server-rs = {
       enable = true;
       settings = {
-        server.listen = ["0.0.0.0:8080" "[::1]:8080"];
+        server.listen = ["127.0.0.1:8080" "[::1]:8080"];
+        # server.tls_listen = ["0.0.0.0:443"];
+
         accounts = {
           auth-type = "htpasswd.default";
           acct-type = "unix";
