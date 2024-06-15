81777dd8fcb98c52840c81c05f52d9127a6ea32c
LilleAila <olai.solsvik@gmail.com>
2024-06-15 09:07:01 +0200

    Installer: fixed typo in gpg command

--- hosts/installer/install.sh
+++ hosts/installer/install.sh
@@ -1,5 +1,5 @@
 # TODO: maybe do inside pkgs.writeShellApplication instead of settings home.file and reading those from here
-gpg --import-key gpg-key.asc
+gpg --import gpg-key.asc
 git clone git@github.com:LilleAila/dotfiles
 cd dotfiles
 git-crypt unlock
