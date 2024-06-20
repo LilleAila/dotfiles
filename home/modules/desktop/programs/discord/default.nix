80738a998278d7bb66151b4883cff0869129b67f
LilleAila <olai.solsvik@gmail.com>
2024-06-20 15:16:48 +0200

    Vesktop: write config directly in nix

--- home/modules/desktop/programs/discord/default.nix
+++ home/modules/desktop/programs/discord/default.nix
@@ -5,11 +5,15 @@
   lib,
   ...
 }: let
-  inherit (lib) mkOption types;
+  jsonFormat = pkgs.formats.json {};
 in {
   options.settings.discord = {
     vesktop.enable = lib.mkEnableOption "vesktop";
     dissent.enable = lib.mkEnableOption "dissent";
+    vesktop.settings = lib.mkOption {
+      type = jsonFormat.type;
+      default = {};
+    };
   };
 
   config = lib.mkMerge [
@@ -46,12 +50,94 @@ in {
           '';
       };
 
-      home.file.".config/vesktop/settings/settings.json".source = ./settings.json;
+      home.file.".config/vesktop/settings/settings.json".source = jsonFormat.generate "vesktop-settings" config.settings.discord.vesktop.settings;
 
-      # Auto-generated color scheme, inspired by https://github.com/deathbeam/base16-discord
-      # Slightly modified by changing theme variables in devtools (Ctrl + Shift + i)
       home.file.".config/vesktop/themes/base16.theme.css".source = pkgs.writeText "base16.theme.css" (import ./theme.nix {inherit config;});
+
+      settings.discord.vesktop.settings = lib.mkDefault {
+        autoUpdate = false;
+        autoUpdateNotification = false;
+        notifyAboutUpdates = false;
+        useQuickCss = true;
+        disableMinSize = true;
+        enabledThemes = ["base16.theme.css"];
+        plugins = {
+          AlwaysTrust.enabled = true;
+          AnonymiseFileNames.enabled = true;
+          BetterUploadButton.enabled = true;
+          CallTimer.enabled = true;
+          ClearURLS.enabled = true;
+          CopyUserURLS.enabled = true;
+          CrashHandler.enabled = true;
+          CtrlEnterSend.enabled = true;
+          DisableCallIdle.enabled = true;
+          EmoteCloner.enabled = true;
+          FakeNitro.enabled = true;
+          FavoriteGifSearch.enabled = true;
+          FixCodeblockGap.enabled = true;
+          FixSpotifyEmbeds.enabled = true;
+          FixYoutubeEmbeds.enabled = true;
+          GifPaste.enabled = true;
+          ImageZoom.enabled = true;
+          # InvisibleChat = {
+          #   enabled = true;
+          #   savedPasswords = "password, Password";
+          # };
+          KeepCurrentChannel.enabled = true;
+          LoadingQuotes.enabled = true;
+          MessageClickActions.enabled = true;
+          MessageLinkEmbeds.enabled = true;
+          MessageLatency.enabled = true;
+          MessageLogger = {
+            enabled = true;
+            ignoreSelf = true;
+          };
+          MessageTags.enabled = true;
+          MoreCommands.enabled = true;
+          MoreKaomoji.enabled = true;
+          MutualGroupDMs.enabled = true;
+          NewGuildSettings.enabled = true;
+          NoF1.enabled = true;
+          NoMosaic.enabled = true;
+          NoPendingCount.enabled = true;
+          NoProfileThemes.enabled = true;
+          NormalizeMessageLinks.enabled = true;
+          NoUnblockToJump.enabled = true;
+          NSFWGateBypass.enabled = true;
+          PermissionsViewer.enabled = true;
+          petpet.enabled = true;
+          PictureInPicture.enabled = true;
+          PreviewMessage.enabled = true;
+          QuickMention.enabled = true;
+          ReadAllNotificationsButton.enabled = true;
+          RelationshipNotifier.enabled = true;
+          RevealAllSpoilers.enabled = true;
+          ReverseImageSearch.enabled = true;
+          ReviewDB.enabled = true;
+          SearchReply.enabled = true;
+          SendTimestamps.enabled = true;
+          ServerInfo.enabled = true;
+          ShikhiCodeblocks.enabled = true;
+          TypingIndicator.enabled = true;
+          TypingTweaks.enabled = true;
+          Unindent.enabled = true;
+          UnlockedAvatarZoom.enabled = true;
+          UrbanDictionary.enabled = true;
+          UserVoiceShow.enabled = true;
+          ValidUser.enabled = true;
+          VencordToolbox.enabled = true;
+          VoiceChatDoubleClick.enabled = true;
+          VoiceMessages.enabled = true;
+          Wikisearch.enabled = true;
+
+          NoTrack.enabled = true;
+          Settings.enabled = true;
+          SupportHelper.enabled = true;
+          WebContextMenus.enabled = true;
+        };
+      };
     })
+
     (lib.mkIf (config.settings.wm.hyprland.enable) {
       wayland.windowManager.hyprland.settings = {
         "$discord" = "vesktop";
