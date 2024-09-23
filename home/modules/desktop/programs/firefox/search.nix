eeec591ea013650adc338d9c416b1d2b75291e8e
LilleAila <olai.solsvik@gmail.com>
2024-09-23 18:40:21 +0200

    firefox: fix nixos wiki search

--- home/modules/desktop/programs/firefox/search.nix
+++ home/modules/desktop/programs/firefox/search.nix
@@ -25,7 +25,7 @@
     };
 
     "NixOS Wiki" = {
-      urls = [ { template = "https://wiki.nixos.org/index.php?search={searchTerms}"; } ];
+      urls = [ { template = "https://wiki.nixos.org/w/index.php?search={searchTerms}"; } ];
       iconUpdateURL = "https://wiki.nixos.org/favicon.png";
       updateInterval = 24 * 60 * 60 * 1000; # Every day
       definedAliases = [ "@nw" ];
