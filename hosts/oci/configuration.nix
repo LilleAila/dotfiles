3fc156b695631d9a8fcb2b609e8fd89b8f611881
LilleAila <olai.solsvik@gmail.com>
2025-02-01 13:56:10 +0100

    oci: add m1pro to trusted ssh keys

--- hosts/oci/configuration.nix
+++ hosts/oci/configuration.nix
@@ -38,6 +38,8 @@
     ssh.keys = with keys.ssh; [
       e14g5.public
       t420.public
+      desktop.public
+      m1pro.public
     ];
     cloudflared.enable = true;
     nextcloud.enable = true;
@@ -52,7 +54,8 @@
     pinentryPackage = pkgs.pinentry-tty;
   };
 
-  services.syncthing.settings.folders."Obsidian Notes".path = lib.mkForce "/home/${config.settings.user.name}/webdav/obsidian";
+  services.syncthing.settings.folders."Obsidian Notes".path =
+    lib.mkForce "/home/${config.settings.user.name}/webdav/obsidian";
 
   # To access the GUI, use SSH tunneling: `ssh -L 9999:localhost:8384 olai@158.179.205.169`, and then go to 127.0.0.1:9999 on the local machine
   # docs: https://docs.syncthing.net/users/firewall.html#tunneling-via-ssh
