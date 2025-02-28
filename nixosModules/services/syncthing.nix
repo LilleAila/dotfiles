8c55cd0fb38fb95d35984ab63bcd29f777c00d8c
LilleAila <olai.solsvik@gmail.com>
2025-02-28 19:25:59 +0100

    syncthing: add config back to nixos
    causes duplicate code. TODO move completely over to home-manager for syncthing config

--- nixosModules/services/syncthing.nix
+++ nixosModules/services/syncthing.nix
@@ -18,33 +18,134 @@ in
   options.settings.syncthing.enable = lib.mkEnableOption "Syncthing";
 
   config = lib.mkMerge [
-    (lib.mkAssert (!config.hm.settings.syncthing.enable) (
-      lib.mkIf config.settings.syncthing.enable {
-        settings.persist.home.directories = [
-          "Documents/Obsidian Vault"
-          "org"
+    (lib.mkIf config.settings.syncthing.enable {
+      settings.persist.home.directories = [
+        "Documents/Obsidian Vault"
+        "org"
 
-          "Sync"
-          ".factorio"
-          ".local/share/PrismLauncher/instances"
-          ".spell"
-        ];
+        "Sync"
+        ".factorio"
+        ".local/share/PrismLauncher/instances"
+        ".spell"
+      ];
 
-        settings.persist.home.cache = [ ".config/syncthing" ];
+      settings.persist.home.cache = [ ".config/syncthing" ];
 
-        # Config panel at http://127.0.0.1:8384/
-        # Go to the config panel to find the device ID
-        services.syncthing = {
-          enable = true;
-          user = username;
-          dataDir = "/home/${username}";
-          configDir = "/home/${username}/.config/syncthing";
-          openDefaultPorts = true;
-          overrideDevices = true;
-          overrideFolders = true;
-          inherit (config.hm.services.syncthing) settings;
+      # Config panel at http://127.0.0.1:8384/
+      # Go to the config panel to find the device ID
+      services.syncthing = {
+        enable = true;
+        user = username;
+        dataDir = "/home/${username}";
+        configDir = "/home/${username}/.config/syncthing";
+        openDefaultPorts = true;
+        overrideDevices = true;
+        overrideFolders = true;
+        # inherit (config.hm.services.syncthing) settings;
+        settings = {
+          options = {
+            urAccepted = -1; # Don't submit usage data
+            relaysEnabled = true;
+          };
+
+          gui = {
+            user = username;
+            inherit (secrets) password;
+          };
+
+          devices = {
+            t420 = {
+              id = secrets.ids.t420;
+            };
+            oci = {
+              id = secrets.ids.oci;
+            };
+            e14g5 = {
+              id = secrets.ids.e14g5;
+            };
+            x220 = {
+              id = secrets.ids.x220;
+            };
+            desktop = {
+              id = secrets.ids.desktop;
+            };
+            m1pro = {
+              id = secrets.ids.m1pro;
+            };
+            m1pro-darwin = {
+              id = secrets.ids.m1pro-darwin;
+            };
+          };
+
+          folders = {
+            "Default Folder" = {
+              path = "${homePath}/Sync";
+              devices = [
+                "oci"
+                "t420"
+                "e14g5"
+                "x220"
+                "desktop"
+                "m1pro"
+                "m1pro-darwin"
+              ];
+            };
+            "Spell" = {
+              path = "${homePath}/.spell";
+              devices = [
+                "oci"
+                "t420"
+                "e14g5"
+                "x220"
+                "desktop"
+                "m1pro"
+                "m1pro-darwin"
+              ];
+            };
+            "Obsidian Notes" = {
+              path = "${homePath}/notes/obsidian";
+              devices = [
+                "oci"
+                "e14g5"
+                "desktop"
+                "m1pro"
+                "m1pro-darwin"
+                "t420"
+              ];
+            };
+            "Calibre Library" = {
+              path = "${homePath}/Calibre\ Library";
+              devices = [
+                "oci"
+                "e14g5"
+                "desktop"
+                "m1pro"
+                "m1pro-darwin"
+                "t420"
+              ];
+            };
+            # TODO: steam sync is probably good now, so switch back to steam-managed factorio
+            "Factorio Saves" = {
+              path = "${homePath}/.factorio";
+              devices = [
+                "oci"
+                "e14g5"
+                "desktop"
+                "m1pro-darwin"
+              ];
+            };
+            "Minecraft" = {
+              path = "${homePath}/.local/share/PrismLauncher/instances";
+              devices = [
+                "oci"
+                "e14g5"
+                "desktop"
+                "m1pro-darwin"
+              ];
+            };
+          };
         };
-      }
-    ))
+      };
+    })
   ];
 }
