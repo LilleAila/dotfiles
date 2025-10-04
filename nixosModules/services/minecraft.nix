013f44233a1ac2da4878402eeed8a581ab1ba166
LilleAila <olai.solsvik@gmail.com>
2025-10-04 11:53:43 +0200

    minecraft: try setting up velocity

--- nixosModules/services/minecraft.nix
+++ nixosModules/services/minecraft.nix
@@ -35,23 +35,18 @@ in
     # TODO. does not work.
     # services.caddy.globalConfig = ''
     #   layer4 {
-    #       :25565 {
-    #         route {
-    #             @minecraft_sni tls {
-    #                 sni minecraft.olai.dev
-    #             }
-    #
-    #             route @minecraft_sni {
-    #                 proxy localhost:25566
-    #             }
-    #
-    #             route {
-    #                 proxy localhost:25566
-    #             }
+    #     :25566 {
+    #       route {
+    #         proxy {
+    #           upstream 127.0.0.1:25565
     #         }
+    #       }
     #     }
-    #
     #   }
+    # '';
+
+    # services.caddy.virtualHosts."minecraft.olai.dev:25565".extraConfig = ''
+    #   reverse_proxy http://127.0.0.1:25565
     # '';
 
     services.minecraft-servers = {
@@ -60,6 +55,27 @@ in
       openFirewall = true;
 
       servers = {
+        # velocity = {
+        #   enable = true;
+        #   package = inputs.nix-minecraft.legacyPackages.${pkgs.system}.velocityServers.velocity;
+        #
+        #   symlinks."velocity.toml" = pkgs.writeText "velocity.toml" ''
+        #     bind = "0.0.0.0:25565"
+        #     motd = "Server"
+        #     forwarding-mode = "modern"
+        #
+        #     [servers]
+        #     main = "127.0.0.1:300001"
+        #
+        #     try = [
+        #       "main"
+        #     ]
+        #
+        #     [forced-hosts]
+        #     minecraft.olai.dev = [ "main" ]
+        #   '';
+        # };
+
         server1 = {
           enable = true;
           enableReload = true; # Reload rather than restart on config change
@@ -69,6 +85,11 @@ in
           symlinks = {
             mods = pkgs.linkFarmFromDrvs "mods" (
               builtins.attrValues {
+                # FabricProxy-Lite = pkgs.fetchurl {
+                #   url = "https://cdn.modrinth.com/data/8dI2tmqs/versions/nR8AIdvx/FabricProxy-Lite-2.11.0.jar";
+                #   sha512 = "c2e1d9279f6f19a561f934b846540b28a033586b4b419b9c1aa27ac43ffc8fad2ce60e212a15406e5fa3907ff5ecbe5af7a5edb183a9ee6737a41e464aec1375";
+                # };
+
                 # TODO https://github.com/henkelmax/hermitcraft-server
                 # (or maybe a variation of it idk)
               }
@@ -79,7 +100,10 @@ in
 
           serverProperties = {
             # https://minecraft.wiki/w/Server.properties
-            port = 25565;
+            server-port = 30001;
+            online-mode = false;
+            server-ip = "127.0.0.1";
+
             gamemode = "survival";
             difficulty = "hard";
             motd = "Veldig gøy server!!";
