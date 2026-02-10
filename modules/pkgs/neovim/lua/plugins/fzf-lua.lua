1e7a8ab5f6c55ecced2d29e937983d6d65dde0ec
LilleAila <olai.solsvik@gmail.com>
2026-02-10 20:15:46 +0100

    neovim/fzf-lua: add tabs keybinds

--- modules/pkgs/neovim/lua/plugins/fzf-lua.lua
+++ modules/pkgs/neovim/lua/plugins/fzf-lua.lua
@@ -5,5 +5,7 @@ return {
   keys = {
     { "<leader>ff", ":FzfLua files<cr>", desc="Files" },
     { "<leader>fs", ":FzfLua live_grep<cr>", desc="Grep" },
+    { "<leader>fb", ":FzfLua buffers<cr>", desc="Buffers" },
+    { "<leader>ft", ":FzfLua tabs<cr>", desc="Tabs" },
   },
 }
