e9febee17628fc8a74894faa8f2c175e5713fdc3
LilleAila <olai.solsvik@gmail.com>
2026-08-16 20:36:44 +0200

    ewm: keybinds

--- modules/nixos/desktop/ewm/ewm-init.el
+++ modules/nixos/desktop/ewm/ewm-init.el
@@ -1,22 +1,31 @@
-; (setq default-input-method "norwegian-keyboard")
-; (setq-default default-input-method "norwegian-keyboard")
-
 (use-package ewm
              :init
+             ;; Reset keymap such that i can define my own in :bind
              (setopt ewm-mode-map (make-sparse-keymap))
              :custom
              (ewm-output-config '(("eDP-1" :width 1920 :height 1200)))
+             (ewm-intercept-prefixes
+               '("C-x" "C-h" "M-x" ; Emacs stuff
+                 "s-:" ; Evil-cmd
+                 ("s-o" :fullscreen) ; Fullscreen
+                 ("<Print>" :fullscreen)))
+             (ewm-input-config
+               '((keyboard :xkb-layouts "no"
+                           :xkb-options "ctrl:nocaps")))
              :config
-             (setopt ewm-input-config
-                     '((keyboard :xkb-layouts "no"
-                                 :xkb-options "ctrl:nocaps")))
             (ewm-text-input-auto-mode-enable)
             :bind (:map ewm-mode-map
-                        ("s-spc" . ewm-launch-app)
-                        ("s-o" . ewm-toggle-fullscreen)
-                        ("s-c" . kill-ring-save)
-                        ("s-v" . yank))
+                        ("s-:" . evil-ex)
+                        ("s-<return>" . (lambda () (interactive)
+                                  (start-process "ghostty" nil "ghostty")))
 
+                        ("s-SPC" . ewm-launch-app)
+                        ("s-o" . ewm-toggle-fullscreen)
+
+                        ("s-c" . kill-ring-save)
+                        ("s-v" . yank)
+
+                        ;; Movement and stuff
                         ("s-h" . ewm-focus-left)
                         ("s-j" . ewm-focus-down)
                         ("s-k" . ewm-focus-up)
@@ -24,10 +33,10 @@
 
                         ("s-t" . ewm-frame-new)
                         ("s-w" . ewm-frame-close)
-                        ("s-S-h" . ewm-frame-left)
-                        ("s-S-l" . ewm-frame-right)
-                        ("C-s-S-h" . ewm-frame-move-left)
-                        ("C-s-S-l" . ewm-frame-move-right)
+                        ("s-H" . ewm-frame-left)
+                        ("s-L" . ewm-frame-right)
+                        ("C-s-H" . ewm-frame-move-left)
+                        ("C-s-L" . ewm-frame-move-right)
 
                         ("s-1" . ewm-frame-select)
                         ("s-2" . ewm-frame-select)
@@ -37,11 +46,4 @@
                         ("s-6" . ewm-frame-select)
                         ("s-7" . ewm-frame-select)
                         ("s-8" . ewm-frame-select)
-                        ("s-9" . ewm-frame-select))
-
-; (add-hook 'after-change-major-mode-hook
-;           (lambda ()
-;             (unless current-input-method
-;               (activate-input-method "norwegian-keyboard"))))
-;
-; (setq evil-want-input-method-and-multilingual-display t)
+                        ("s-9" . ewm-frame-select)))
