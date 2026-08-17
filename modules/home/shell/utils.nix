a9dcd012866abb6e7a8f72631e79e07c7b70a5be
LilleAila <olai.solsvik@gmail.com>
2026-08-17 22:44:05 +0200

    utils/ssh: migrate matchBlocks to settings

--- modules/home/shell/utils.nix
+++ modules/home/shell/utils.nix
@@ -50,18 +50,18 @@
           enable = true;
           enableDefaultConfig = false;
           # NOTE: this is a user-specific setting, that should maybe be set somewhere else. desktop/default.nix?
-          matchBlocks = self.secrets.ssh-hosts // {
+          settings = self.secrets.ssh-hosts // {
             "*" = {
-              forwardAgent = false;
-              addKeysToAgent = "no";
-              compression = false;
-              serverAliveInterval = 0;
-              serverAliveCountMax = 3;
-              hashKnownHosts = false;
-              userKnownHostsFile = "~/.ssh/known_hosts";
-              controlMaster = "no";
-              controlPath = "~/.ssh/master-%r@%n:%p";
-              controlPersist = "no";
+              ForwardAgent = false;
+              AddKeysToAgent = "no";
+              Compression = false;
+              ServerAliveInterval = 0;
+              ServerAliveCountMax = 3;
+              HashKnownHosts = false;
+              UserKnownHostsFile = "~/.ssh/known_hosts";
+              ControlMaster = "no";
+              ControlPath = "~/.ssh/master-%r@%n:%p";
+              ControlPersist = "no";
             };
           };
         };
